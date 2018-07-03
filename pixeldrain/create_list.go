7b6eb971f2e494593b1ffa0ae054f400a8ca6fe0
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2018-07-03 13:53:13 -0400

    feat: impalement CreateList function

---
+++ pixeldrain/create_list.go
@@ -0,0 +1,53 @@
+// create_list.go
+//
+// Copyright (c) 2018 Junpei Kawamoto
+//
+// This software is released under the MIT License.
+//
+// http://opensource.org/licenses/mit-license.php
+
+package pixeldrain
+
+import (
+	"context"
+	"strings"
+
+	"github.com/jkawamoto/go-pixeldrain/client"
+	"github.com/jkawamoto/go-pixeldrain/client/list"
+	"github.com/jkawamoto/go-pixeldrain/models"
+)
+
+// CreateList sends a list creation request via the given client with the given title, description, and items.
+func CreateList(ctx context.Context, cli *client.PixelDrain, title, description string, items []string) (id string, err error) {
+
+	res, err := cli.List.PostList(list.NewPostListParamsWithContext(ctx).WithList(
+		&models.PostListParamsBody{
+			Title:       &title,
+			Description: description,
+			Files:       ParseListItems(items),
+		},
+	))
+	if err != nil {
+		return
+	}
+
+	id = res.Payload.ID
+	return
+
+}
+
+// ParseListItems parses the given list of list specifications and returns a PostListParamsBodyFiles instance.
+func ParseListItems(values []string) (res models.PostListParamsBodyFiles) {
+
+	for _, v := range values {
+		c := strings.SplitN(v, ":", 2)
+		item := &models.PostListParamsBodyFilesItems{
+			ID: c[0],
+		}
+		if len(c) != 1 {
+			item.Description = c[1]
+		}
+		res = append(res, item)
+	}
+	return
+}
---
+++ pixeldrain/create_list_test.go
@@ -0,0 +1,163 @@
+// create_list_test.go
+//
+// Copyright (c) 2018 Junpei Kawamoto
+//
+// This software is released under the MIT License.
+//
+// http://opensource.org/licenses/mit-license.php
+
+package pixeldrain
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/go-openapi/swag"
+	"github.com/jkawamoto/go-pixeldrain/client"
+	"github.com/jkawamoto/go-pixeldrain/models"
+)
+
+type mockListServer struct {
+	ID          string
+	Description string
+	Title       string
+	Files       models.PostListParamsBodyFiles
+}
+
+func (m *mockListServer) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+
+	res.Header().Add("Content-type", "application/json")
+	if req.URL.Path != "/list" {
+		res.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(res).Encode(models.StandardError{Message: swag.String("received a wrong request")})
+		return
+	}
+
+	raw, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(res).Encode(models.StandardError{
+			Message: swag.String(fmt.Sprintln("failed to parse the request:", err)),
+		})
+		return
+	}
+
+	var body models.PostListParamsBody
+	err = body.UnmarshalBinary(raw)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(res).Encode(models.StandardError{
+			Message: swag.String(fmt.Sprintln("failed to parse the request:", err)),
+		})
+		return
+	}
+
+	m.Description = body.Description
+	m.Title = *body.Title
+	m.Files = body.Files
+
+	json.NewEncoder(res).Encode(&models.PostListOKBody{
+		ID:      m.ID,
+		Success: true,
+	})
+	return
+
+}
+
+func TestCreateList(t *testing.T) {
+
+	mock := &mockListServer{
+		ID: "sample-id",
+	}
+	server := httptest.NewServer(mock)
+	defer server.Close()
+
+	u, err := url.Parse(server.URL)
+	if err != nil {
+		t.Fatal("Cannot parse a URL:", err)
+	}
+	cli := client.NewHTTPClientWithConfig(nil, &client.TransportConfig{
+		Host:     u.Host,
+		BasePath: "/",
+		Schemes:  []string{"http"},
+	})
+
+	title := "sample-title"
+	description := "sample-description"
+	files := []string{"file1:file1.txt", "file2:file2.dat"}
+	id, err := CreateList(context.Background(), cli, title, description, files)
+	if err != nil {
+		t.Fatal("CreateList returned an error:", err)
+	}
+
+	if id != mock.ID {
+		t.Errorf("received id was %v but expected %v", id, mock.ID)
+	}
+	if len(mock.Files) != len(files) {
+		t.Errorf("the mock server received %v items but sent %v items", len(mock.Files), len(files))
+	} else {
+		for i, f := range ParseListItems(files) {
+			if item := mock.Files[i]; item.ID != f.ID || item.Description != f.Description {
+				t.Errorf("item %v was %+v but expected %+v", i, item)
+			}
+		}
+	}
+
+}
+
+func TestParseListItems(t *testing.T) {
+
+	cases := []struct {
+		Input    []string
+		Expected models.PostListParamsBodyFiles
+	}{
+		{
+			Input: []string{"ID"},
+			Expected: models.PostListParamsBodyFiles{
+				&models.PostListParamsBodyFilesItems{ID: "ID"},
+			},
+		},
+		{
+			Input: []string{"ID:description"},
+			Expected: models.PostListParamsBodyFiles{
+				&models.PostListParamsBodyFilesItems{ID: "ID", Description: "description"},
+			},
+		},
+		{
+			Input: []string{"ID:description", "id2"},
+			Expected: models.PostListParamsBodyFiles{
+				&models.PostListParamsBodyFilesItems{ID: "ID", Description: "description"},
+				&models.PostListParamsBodyFilesItems{ID: "id2"},
+			},
+		},
+		{
+			Input: []string{"ID:description", "id2:desc2"},
+			Expected: models.PostListParamsBodyFiles{
+				&models.PostListParamsBodyFilesItems{ID: "ID", Description: "description"},
+				&models.PostListParamsBodyFilesItems{ID: "id2", Description: "desc2"},
+			},
+		},
+	}
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("case-%v", i), func(t *testing.T) {
+			res := ParseListItems(c.Input)
+			if len(res) != len(c.Expected) {
+				t.Errorf("got %v items but want %v", len(res), len(c.Expected))
+			} else {
+				for j, e := range c.Expected {
+					if res[j].ID != e.ID || res[j].Description != e.Description {
+						t.Errorf("item %v: %+v but want ID = %v, Description = %q", j, res[j], e.ID, e.Description)
+					}
+				}
+			}
+		})
+	}
+
+}
